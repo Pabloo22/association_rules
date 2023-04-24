807f32f3c5274675376acc0d9a5344c0f0a36a25
Pabloo22 <pablete.arino@gmail.com>
2023-04-24 18:35:35 +0200

    Fix make_dataset

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for src."""
 
-__author__ = """Pablo Ariñoi and Alvaro Laguna"""
+__author__ = """Pablo Arino & Alvaro Laguna"""
 __email__ = 'pablo.arino@alumnos.upm.es'
 __version__ = '0.1.0'
+
+from .data.make_dataset import get_raw_data
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
 import pandas as pd
+import dotenv
 
 
-def get_raw_data():
-    """Get raw data from data/raw/ directory."""
-    return pd.read_csv("data/raw/iris.csv")
+def setup_project():
+    """ Setup project path and load environment variables """
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    project_path = dotenv.get_key(dotenv.find_dotenv(), "project_path")
+    os.chdir(project_path)
+
+
+def get_raw_data(filename: str = "movies"):
+    setup_project()
+    return pd.read_csv(f"data/raw/{filename}.csv")
+
+
+if __name__ == '__main__':
+    pass
